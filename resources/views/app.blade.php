3b755dcc7467d5d6ae17e56495402d8908bddcec
Esieba Okeoghene Diamond <37224358+MacroTom@users.noreply.github.com>
2023-12-06 21:06:56 +0000

    advancements

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     @if (config("app.env") === "local" && env('APP_DOCKER') == 'true')
-        <link rel="stylesheet" href="{{env('APP_URL')}}/build/{{getAssets()['css']}}">
-        <script src="{{env('APP_URL')}}/build/{{getAssets()['js']}}" type="module"></script>
+        <link rel="stylesheet" href="/build/{{getAssets()['css']}}">
+        <script src="/build/{{getAssets()['js']}}" type="module"></script>
     @else
         @vite('resources/js/app.js')
     @endif
